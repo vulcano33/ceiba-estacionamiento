f85620ae87de4a3aac415fb20a6033f04a01e4ee
Carlos Arturo Cabrera Camacho - Ceiba Software <carlos.cabrera@ceiba.com.co>
2018-09-03 07:09:50 -0500

    Initial commit

---
+++ src/test/java/co/com/ceiba/estacionamiento/carlos/cabrera/estacionamientoceiba/EstacionamientoCeibaApplicationTests.java
@@ -0,0 +1,16 @@
+package co.com.ceiba.estacionamiento.carlos.cabrera.estacionamientoceiba;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EstacionamientoCeibaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
