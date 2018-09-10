1d75a677b46b5c8991fec76e4a89066d3553b3a7
Carlos Arturo Cabrera Camacho - Ceiba Software <carlos.cabrera@ceiba.com.co>
2018-09-10 15:40:16 -0500

    Remove some useless tests

--- src/test/java/co/com/ceiba/estacionamiento/carlos/cabrera/estacionamientoceiba/controlador/ParqueaderoControladorTest.java
+++ src/test/java/co/com/ceiba/estacionamiento/carlos/cabrera/estacionamientoceiba/controlador/ParqueaderoControladorTest.java
@@ -2,14 +2,11 @@ package co.com.ceiba.estacionamiento.carlos.cabrera.estacionamientoceiba.control
 
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
-
 import java.io.IOException;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -17,7 +14,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ParqueaderoControladorTest {
 
 	@Autowired
